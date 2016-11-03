07f4186c3eb0208b8d977da05da37550d995bcd0
Parth Jansari <parthjansari@outlook.com>
2016-11-03 10:43:14 +0530

    added window.requestAnimationFrame()
    mouse move works with fps30+but broke the plugin!
    refactoring the code to solve the error

--- _assets/_javascripts/app.js
+++ _assets/_javascripts/app.js
@@ -1,57 +1,67 @@
 var bg = $('.bg-icon'),
     count = 0;
-    width = $(document).width(),
+width = $(document).width(),
     height = $(document).height(),
     w_height = $(window).height(),
     w_width = $(window).width(),
     svg_list = ["icon-01.svg", "icon-02.svg", "icon-03.svg", "icon-04.svg", "icon-05.svg", "icon-06.svg", "icon-07.svg", "icon-08.svg", "icon-09.svg", "icon-10.svg", "icon-11.svg", "icon-12.svg"],
-    max_icon = 30,
-    last_Pos = ($(".last").offset().top)-600,
+    max_icon = 40,
+    last_Pos = ($(".last").offset().top) - 600,
     h_ratio = (height / w_height) * 100;
-    w_ratio = (width / w_width) * 100;
+w_ratio = (width / w_width) * 100;
 bg.height(last_Pos + "px");
 $(window).resize(function(event) {
-  width = ($(document).width())+17
-  bg_icon_move()
+    width = ($(document).width()) + 17
+    bg_icon_move()
 });
 for (var i = 0; i < max_icon; i++) {
-    bg.append("<div class='icon'style='left: calc(" + rand_range(25,75,100) + "% - 60px); top: " + rand_gen(h_ratio) + "%;background-image: url(../img/svg/" + svg_list[rand_gen(11)] + ");'></div>");
+    bg.append("<div class='icon'style='left: calc(" + rand_range(25, 75, 100) + "% - 60px); top: " + rand_gen(h_ratio) + "%;background-image: url(../img/svg/" + svg_list[rand_gen(11)] + ");'></div>");
 }
 $(document).ready(function() {
-    bg_icon_move();
+    window.requestAnimationFrame(bg_icon_move)
     scrollknight.scroll($(".tip ul li"), 300, "active")
     scrollknight.peralax()
     animations_page();
 });
+
 function bg_icon_move() {
-  $(window).mousemove(function(event) {
-  if (width > 800){
-      // this.style.willChange = 'transform, opacity';
-        var x = event.pageX;
-        var y = event.pageY;
-        x = (((x / width) - 0.5) * 90)
-        y = (((y / height) - 0.5) * 90)
-        TweenMax.to(".dir1", 0.05,{transform:"translate(" + x + "px," + y + "px)"})
-        TweenMax.to(".dir2", 0.05,{transform:"translate(" + (x * -1) + "px," + (y * -1) + "px)"})
-        // $(".dir1").css('transform', "translate(" + x + "px," + y + "px)");
-        // $(".dir2").css('transform', "translate(" + (x * -1) + "px," + (y * -1) + "px)");
-      }
+    $(window).mousemove(function(event) {
+        if (width > 800) {
+            window.requestAnimationFrame(bg_icon_move)
+            var x = event.pageX;
+            var y = event.pageY;
+            x = (((x / width) - 0.5) * 90)
+            y = (((y / height) - 0.5) * 90)
+            // TweenMax.to(".dir1", 0.05, {
+            //     transform: "translate(" + x + "px," + y + "px)"
+            // })
+            // TweenMax.to(".dir2", 0.05, {
+            //         transform: "translate(" + (x * -1) + "px," + (y * -1) + "px)"
+            //     })
+                  $(".dir1").css('transform', "translate(" + x + "px," + y + "px)");
+                  $(".dir2").css('transform', "translate(" + (x * -1) + "px," + (y * -1) + "px)");
+            $(this).unbind(event);
+        }
     });
 }
+
 function rand_gen(val) {
     return Math.floor((Math.random() * val) + 1);
 }
-function rand_range(val1,val2,val3) {
+
+function rand_range(val1, val2, val3) {
     var bool = Math.floor((Math.random() * 2));
     if (bool == 1) {
-      return Math.floor((Math.random() * val1) + 1);
-    }
-    else {
-      return Math.floor((Math.random() * (val3-val2)) + val2);
+        return Math.floor((Math.random() * val1) + 1);
+    } else {
+        return Math.floor((Math.random() * (val3 - val2)) + val2);
     }
 }
+
 function svg_display() {
-  TweenMax.set(".icon", {opacity:0});
+    TweenMax.set(".icon", {
+        opacity: 0
+    });
     bg.children('.icon:odd').addClass('dir1')
     bg.children('.icon:even').addClass('dir2')
     bg.children('.icon').css('display', 'block');
---
+++ _assets/_javascripts/pluginpure.js
@@ -0,0 +1,79 @@
+var p = 0,
+obj_array=[],
+rate_array=[],
+i_array=[],
+peralax_call = 0,
+scrollknight;
+scrollknight = {
+peralax: function() {
+    peralax_call = 1
+},
+scroll : function (menu, offset, active_class) {
+    var l_id,
+        menu_height = menu.clientHeight,
+        menu_items = menu.find("a");
+        console.log(menu);
+        scroll_div = menu_items.map(function() {
+        var item = $($(this).attr("href"));
+        if (item.length) {
+            return item;
+        }
+    });
+    menu.click(function(event) {
+        /* Act on the event */
+        event.preventDefault()
+        var scroll = $(this).children('a').attr('href');
+        var scroll_dist = ($(scroll).offset().top) - offset
+            //  $(window).scroll(scroll_dist)
+        TweenMax.to(window, 0.3, {
+            scrollTo: scroll_dist
+        });
+    });
+    peralaxknight();
+    $(window).scroll(function() {
+      win_scroll(offset,active_class,menu_height,menu_items,scroll_div,l_id)
+    if (peralax_call =! 0) {
+      peralax_divs.map(function(elem) {
+          TweenMax.to(this, 0.001,{ease: Power4.easeInOut,transform:"translate"+peralax_dir[elem]+"("+$(window).scrollTop()/peralax_rate[elem]+'px)'});
+      })
+    }
+    });
+},
+}
+function peralaxknight() {
+    peralax_divs = $(document).find(".peralax");
+    peralax_rate = peralax_divs.map(function(elem) {
+        var data = $(this).attr('data-peralax-rate');
+        if (data == undefined)
+        {
+          data = 1.5
+        }
+        return data;
+    })
+    peralax_dir = peralax_divs.map(function(elem) {
+        var data = $(this).attr('data-peralax-dir').toUpperCase();
+        if (data == undefined)
+        {
+          throw "----> No direction found on peralax_div number----> "+ (elem+1);
+        }
+        else if (data != "X" && data != "Y") {
+          throw "----> No velid direction found on peralax_div number----> "+ (elem+1);
+        }
+        return data;
+    })
+}
+function win_scroll(offset,active_class,menu_height,menu_items,scroll_div,l_id){
+  var dist = $(this).scrollTop() + menu_height + offset;
+  var cur = scroll_div.map(function() {
+      if ($(this).offset().top < dist)
+          return this;
+  });
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : "";
+  if (l_id !== id) {
+      l_id = id;
+      menu_items
+          .parent().removeClass(active_class)
+          .end().filter("[href='#" + id + "']").parent().addClass(active_class);
+  }
+}
